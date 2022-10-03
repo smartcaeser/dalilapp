78ddefda0fe8608269ffd348e4a53d2f9c35d9e3
mabdo <4fBkbH9BeV24CvwS>
2022-10-04 01:25:17 +0200

    test contact

--- routes/v1/contactApi.js
+++ routes/v1/contactApi.js
@@ -48,7 +48,7 @@ router.get('/', (req, res) => {
 
         service.getAllWithIncludes(contactDaos => {
             var contactDtos = contactDaos.map(contactDao => wrapper.createDTO(contactDao));
-			console.log(contactDtos);
+			contactDtos = contactDtos.filter(item => item.contactId == '00818500-712c-11eb-a707-cd8b0f7b0e2e');
             responseUtility.createSuccessResponse(res, contactDtos);
         });
 
